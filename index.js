a65f4c56f8d734480282bca9b1f60e596e712d45
ViniciusLucca <vinilucca7304@gmail.com>
2025-02-24 18:55:02 -0300

    Write pseudocode

---
+++ index.js
@@ -0,0 +1,22 @@
+// Computer's choice
+// - Randomly generate choice
+// - Return one of three: "Rock", "Paper", "Scissors" 
+
+// Human's choice
+// - Get user input
+// - Capitalize text
+// - If word is not one of: "Rock", "Paper", "Scissors" 
+// --- Return: "[user's input] is not a valid choice!"
+// - Else
+// --- Return user's capitalized input
+
+// Players score values
+// - Declare variables for Computer and User's scores 
+
+// Logic to play 1 round
+// - Get Computer and User choices
+// - Compare choices
+// - Increase winner's score by 1
+
+// Logic to play 5 rounds
+// - Repeat [Logic to play 1 round] 5 times
