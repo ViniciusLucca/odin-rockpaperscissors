090313261709db0b865b8f8e321ea36502893ca9
ViniciusLucca <vinilucca7304@gmail.com>
2025-02-24 22:04:45 -0300

    Change strings capitalization
    - Changed strings capitalization to lowercase because it is easier to work with
    - Add input verification

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ function getComputerChoice(){
     // - Randomly generate choice
     let randomNumber = Math.random();
     // - Return one of three: "Rock", "Paper", "Scissors" 
-    if(randomNumber > 0) return "Rock";
-    else if(randomNumber == 0) return "Paper";
-    else if(randomNumber < 0) return "Scissors";
+    if(randomNumber > 0) return "rock";
+    else if(randomNumber == 0) return "paper";
+    else if(randomNumber < 0) return "scissors";
 }
 
 
 // Human's choice
-// - Get user input
-// - Capitalize text
-// - If word is not one of: "Rock", "Paper", "Scissors" 
-// --- Return: "[user's input] is not a valid choice!"
-// - Else
-// --- Return user's capitalized input
+function getPlayerChoice(){
+    // - Get user input
+    // - Lowercase text
+    let choice;
+    do{
+        (window.prompt("Choose: Rock, Paper, Scissors")).toLowerCase();
+    } while (choice != "rock" && choice != "paper" && choice != "scissors");
+    // - If word is not one of: "Rock", "Paper", "Scissors"
+    // --- Return: "[user's input] is not a valid choice!"
+    // - Else
+    // --- Return user's lowercased input
+}
 
 // Players score values
 // - Declare variables for Computer and User's scores 
